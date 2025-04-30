48e964ac01aa4e4c6cdc9330e79668d6a08ab51c
Aaron Mizrachi <dev@unmanarc.com>
2025-05-01 01:43:40 +0300

    Refactor login.js and code improvements

--- var/www/ufastauthd2_login/local/js/login.js
+++ var/www/ufastauthd2_login/local/js/login.js
@@ -1,6 +1,49 @@
+// Helpers:
 
+function updateMessage(text) {
+    $("#message").text(text);
+}
+
+function showError(error){
+    updateMessage(`${error}`);
+}
+
+function showErrorWithDetails(error, message){
+    showError(`${error}: ${message}`);
+}
+
+function showErrorWithXHR(xhr, status, error) {
+    showErrorWithDetails(error, xhr.responseJSON["message"]);
+}
+
+// Helper to create and submit form
+function createAndSubmitRedirectForm(actionUrl, data, method = 'POST') {
+    const form = document.createElement('form');
+    form.method = method;
+    form.action = actionUrl;
+    Object.entries(data).forEach(([name, value]) => {
+        const input = document.createElement('input');
+        input.type = 'hidden';
+        input.name = name;
+        input.value = value;
+        form.appendChild(input);
+    });
+    document.body.appendChild(form);
+    form.submit();
+}
+
+////////////////////////////////////////////////////////////////////////////////////
+
+let decodedRedirectURI = "";
 let loggedIn = false;
 let loggedInParamx = true;
+
+const urlParams = new URLSearchParams(window.location.search);
+
+// Retrieve the 'redirectURI' and 'app' parameter from the URL
+const mode = urlParams.get('mode');
+const appName = urlParams.get('app');
+const encodedRedirectURI = urlParams.get('redirectURI');
 
 function logout() {
     $.ajax({
@@ -9,89 +52,33 @@ function logout() {
         contentType: "application/json",
         data: JSON.stringify({}),
         success: function (response) {
-            console.log("Logout success:", response);
             // Recargar la página con los mismos parámetros GET
             loggedIn = false;
-            //alert("logged out");
-            location.href = window.location.href;
+            location.reload();
         },
-        error: function (xhr, status, error) {
-            console.error("Error during logout:", status, error);
-            console.log(status);
-            // Set message:
-            $('#message').text(`${error}: ${xhr.responseJSON["message"]}`);
-        }
+        error: showErrorWithXHR
     });
 }
 
-function reloadTokenAndRedirect() 
+function retokenizeApplication() 
 {
-    // Retrieve the 'redirectURI' and 'app' parameter from the URL
-    const urlParams = new URLSearchParams(window.location.search);
-    const encodedRedirectURI = urlParams.get('redirectURI');
-    const appName = urlParams.get('app');
-    const mode = urlParams.get('mode');
-
-    // Decode the base64 parameter
-    let decodedRedirectURI = "";
-    try {
-        decodedRedirectURI = atob(encodedRedirectURI);
-    } catch (error) {
-        console.error("Error decoding redirectURI parameter:", error);
-        $('#message').text("Invalid redirect URI");
-        return;
-    }
-
     // Perform the AJAX request
     $.ajax({
         url: "/api/v1/retokenize",
         type: "POST",
         contentType: "application/json",
-        data: JSON.stringify({ redirectURI: decodedRedirectURI }),
+        data: JSON.stringify({ 
+            redirectURI: decodedRedirectURI,
+            app: appName
+        }),
         success: function (response) {
-            console.log("App Token Adquired:", response);
-
-            // Perform redirection via POST
-            const form = document.createElement('form');
-
-            if (mode === 'app') 
-            {
-                form.method = "GET";
-            }
-            else
-            {
-                form.method = "POST";
-            }
-            form.action = response.callbackURI;
-
-            // Include the accessToken in the POST body
-            const input = document.createElement('input');
-            input.type = "hidden";
-            input.name = "accessToken";
-            input.value = response.accessToken;
-
-            const input2 = document.createElement('input2');
-            input2.type = "hidden";
-            input2.name = "expiresIn";
-            input2.value = response.expiresIn;
-
-            const input3 = document.createElement('input3');
-            input3.type = "hidden";
-            input3.name = "redirectURI";
-            input3.value = decodedRedirectURI;
-
-            form.appendChild(input);
-            form.appendChild(input2);
-            form.appendChild(input3);
-            document.body.appendChild(form);
-            form.submit();
+            createAndSubmitRedirectForm(response.callbackURI, {
+                accessToken: response.accessToken,
+                expiresIn: response.expiresIn,
+                redirectURI: decodedRedirectURI
+            }, mode === 'app' ? 'GET' : 'POST');
         },
-        error: function (xhr, status, error) {
-            console.error("Error during authorization:", status, error);
-
-            // Set error message
-            $('#message').text(`${error}: ${xhr.responseJSON["message"]}`);
-        }
+        error: showErrorWithXHR
     });
 }
 
@@ -100,24 +87,7 @@ function redirectToAuthenticatedSite() {
     if (loggedInParamx === false)
     {
         // Reinject the application token.
-        reloadTokenAndRedirect();
-        return;
-    }
-
-
-    // Retrieve the 'redirectURI' and 'app' parameter from the URL
-    const urlParams = new URLSearchParams(window.location.search);
-    const encodedRedirectURI = urlParams.get('redirectURI');
-    const appName = urlParams.get('app');
-    const mode = urlParams.get('mode');
-
-    // Decode the base64 parameter
-    let decodedRedirectURI = "";
-    try {
-        decodedRedirectURI = atob(encodedRedirectURI);
-    } catch (error) {
-        console.error("Error decoding redirectURI parameter:", error);
-        $('#message').text("Invalid redirect URI");
+        retokenizeApplication();
         return;
     }
 
@@ -131,44 +101,13 @@ function redirectToAuthenticatedSite() {
             redirectURI: decodedRedirectURI 
         }),
         success: function (response) {
-            console.log("App Token Adquired:", response);
-
-            // Perform redirection via POST
-            const form = document.createElement('form');
-
-            if (mode === 'app') 
-            {
-                form.method = "GET";
-            }
-            else
-            {
-                form.method = "POST";
-            }
-            form.action = response.callbackURI;
-
-            // Include the accessToken in the POST body
-            const input = document.createElement('input');
-            input.type = "hidden";
-            input.name = "accessToken";
-            input.value = ""; // Keep the current/used access token.
-
-            const input3 = document.createElement('input3');
-            input3.type = "hidden";
-            input3.name = "redirectURI";
-            input3.value = decodedRedirectURI;
-
-            form.appendChild(input);
-            form.appendChild(input3);
-
-            document.body.appendChild(form);
-            form.submit();
+                // submit the token to the app.
+                createAndSubmitRedirectForm(response.callbackURI, {
+                    accessToken: "", // No access token means that the application will use the current one.
+                    redirectURI: decodedRedirectURI // Will only accept this from the admited Origin URL.
+                }, mode === 'app' ? 'GET' : 'POST');
         },
-        error: function (xhr, status, error) {
-            console.error("Error during authorization:", status, error);
-
-            // Set error message
-            $('#message').text(`${error}: ${xhr.responseJSON["message"]}`);
-        }
+        error: showErrorWithXHR
     });
 }
 
@@ -181,12 +120,8 @@ $(document).ready(function () {
     // Focus on username input when the page loads
     $("#username").focus();
 
-    $("#message").text('Please enter your username');
+    updateMessage('Please enter your username');
 
-    // Validate if 'redirectURI' exists and is a valid base64 string
-    const urlParams = new URLSearchParams(window.location.search);
-    const encodedRedirectURI = urlParams.get('redirectURI');
-    const appName = urlParams.get('app');
     const loggedInParam = urlParams.get('loggedIn');
 
     if (loggedInParam === "false")
@@ -198,37 +133,20 @@ $(document).ready(function () {
         loggedInParamx = false;
     }
 
-  /*  
-    now permit empty redirect URI.
-    if (!encodedRedirectURI) 
-    {
-        console.log('invalid redirectURI parameter.');
-        console.log(urlParams);
-        console.log(window.location.search);
-        console.log(encodedRedirectURI);
-
-        $('#message').text("Invalid redirect URI...");
-        $("#usernameForm").addClass("d-none");
-        return;
-    }*/
-
     if (!appName)
     {
-        console.log('invalid app');
-        console.log(urlParams);
-        console.log(window.location.search);
-        console.log(encodedRedirectURI);
-
-        $('#message').text("Invalid Application Name...");
+        updateMessage('ERROR: Invalid Application Name.');
         $("#usernameForm").addClass("d-none");
         return;
     }
 
+    // Validate if 'redirectURI' exists and is a valid base64 string
     try {
-        atob(encodedRedirectURI); // Test decoding to ensure valid base64
-    } catch (error) {
-        console.error("Error decoding redirectURI parameter:", error);
-        $('#message').text("Invalid redirect URI...");
+        decodedRedirectURI = atob(encodedRedirectURI);
+    } 
+    catch (error) 
+    {
+        updateMessage('ERROR: Invalid redirect URI.');
         $("#usernameForm").addClass("d-none");
         return;
     }
@@ -237,21 +155,18 @@ $(document).ready(function () {
     const cookies = document.cookie.split(';');
     cookies.forEach(cookie => {
         const [name, value] = cookie.trim().split('=');
-
         if (name === 'loggedIn')
         {
             loggedIn = true;
 
             $("#usernameForm").addClass("d-none");
             $("#logoutForm").removeClass("d-none");
-            $("#message").text('Already logged in');
-
+            updateMessage('Already logged in.');
         }
     });
 
     // Function to initialize authentication flow using preAuthorize API
     function initializeAuthentication(username) {
-        const appName = urlParams.get('app');
         $.ajax({
             url: "/api/v1/preAuthorize",
             type: "POST",
@@ -266,7 +181,7 @@ $(document).ready(function () {
                 const scheme = response.availableSchemes[defaultSchemeIndex];
 
                 if (!scheme) {
-                    alert("No available authentication scheme found.");
+                    updateMessage('ERROR: No available authentication scheme found.');
                     return;
                 }
 
@@ -275,32 +190,11 @@ $(document).ready(function () {
 
                 showNextSlot();
             },
-            error: function (xhr, status, error) {
-                console.error("Error calling preAuthorize API:", status, error);
-                alert("Failed to initiate authentication. Please try again.");
-            }
+            error: showErrorWithXHR
         });
     }
 
-
-
     function loadTokenAndRedirect() {
-        // Retrieve the 'redirectURI' and 'app' parameter from the URL
-        const urlParams = new URLSearchParams(window.location.search);
-        const encodedRedirectURI = urlParams.get('redirectURI');
-        const appName = urlParams.get('app');
-        const mode = urlParams.get('mode');
-    
-        // Decode the base64 parameter
-        let decodedRedirectURI = "";
-        try {
-            decodedRedirectURI = atob(encodedRedirectURI);
-        } catch (error) {
-            console.error("Error decoding redirectURI parameter:", error);
-            $('#message').text("Invalid redirect URI");
-            return;
-        }
-    
         // Perform the AJAX request
         $.ajax({
             url: "/api/v1/token",
@@ -308,67 +202,19 @@ $(document).ready(function () {
             contentType: "application/json",
             data: JSON.stringify({ redirectURI: decodedRedirectURI }),
             success: function (response) {
-                console.log("App Token Adquired:", response);
-    /*
-                // Prepare JSON payload for POST body
-                const payload = {
-                    access_token: response.accessToken,
+                // submit the token to the app.
+                createAndSubmitRedirectForm(response.callbackURI, {
+                    accessToken: response.accessToken,
                     expiresIn: response.expiresIn,
-                    app_name: appName,
-    
-                    token_type: "Bearer"
-                };*/
-    
-                // Perform redirection via POST
-                const form = document.createElement('form');
-    
-                if (mode === 'app') 
-                {
-                    form.method = "GET";
-                }
-                else
-                {
-                    form.method = "POST";
-                }
-                form.action = response.callbackURI;
-    
-                // Include the accessToken in the POST body
-                const input = document.createElement('input');
-                input.type = "hidden";
-                input.name = "accessToken";
-                input.value = response.accessToken;
-    
-                const input2 = document.createElement('input2');
-                input2.type = "hidden";
-                input2.name = "expiresIn";
-                input2.value = response.expiresIn;
-    
-                const input3 = document.createElement('input3');
-                input3.type = "hidden";
-                input3.name = "redirectURI";
-                input3.value = decodedRedirectURI;
-    
-                form.appendChild(input);
-                form.appendChild(input2);
-                form.appendChild(input3);
-                document.body.appendChild(form);
-                form.submit();
-            // alert("ok, ya");
+                    redirectURI: decodedRedirectURI
+                }, mode === 'app' ? 'GET' : 'POST');
             },
-            error: function (xhr, status, error) {
-                console.error("Error during authorization:", status, error);
-    
-                // Set error message
-                $('#message').text(`${error}: ${xhr.responseJSON["message"]}`);
-            }
+            error: showErrorWithXHR
         });
     }
 
-
     // Function to send authorization request for each slot
     function authorizeUser(username, schemeId, password) {
-        const appName = urlParams.get('app');
-
         $.ajax({
             url: "/api/v1/authorize",
             type: "POST",
@@ -386,7 +232,7 @@ $(document).ready(function () {
 
                 if (response.isFullyAuthenticated) {
                     loggedIn = true;
-                    $("#message").text("Authenticated! Redirecting...");
+                    updateMessage("Authenticated! Redirecting...");
                     loadTokenAndRedirect();
                 } else {
                     currentSlotIndex++;
@@ -394,12 +240,8 @@ $(document).ready(function () {
                 }
             },
             error: function (xhr, status, error) {
-                console.error("Error during authorization:", status, error);
-                console.log(status);
+                showErrorWithXHR(xhr,status,error);
                 loggedIn = false;
-                // Set message:
-                //console.log(`${error}: ${xhr.responseJSON["message"]}`);
-                $('#message').text(`${error}: ${xhr.responseJSON["message"]}`);
             }
         });
     }
@@ -407,7 +249,7 @@ $(document).ready(function () {
     // Update the content based on the current slot
     function showNextSlot() {
         if (currentSlotIndex >= schemes.length) {
-            $("#message").text("Authenticated! Redirecting...");
+            updateMessage("Authenticated! Redirecting...");
             loadTokenAndRedirect();
             // Here we have to get the application token
             return;
@@ -416,7 +258,7 @@ $(document).ready(function () {
         const slot = schemes[currentSlotIndex];
         const { description, passwordFunction } = slot.details;
 
-        $("#message").text("Please enter your " + description);
+        updateMessage("Please enter your " + description);
 
         if (passwordFunction === 5) {
             // OTP input
