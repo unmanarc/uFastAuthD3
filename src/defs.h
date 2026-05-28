b275893fa278882686c54eec3d132c97eb60fe28
Aaron Mizrachi <dev@unmanarc.com>
2026-05-29 02:29:21 +0300

    fix: shorten IAM_USRPORTAL_DESCRIPTION by removing "And Login Page" suffix

--- src/defs.h
+++ src/defs.h
@@ -7,7 +7,7 @@
 #define IAM_ADMPORTAL_URL "https://iamadmin.localhost:9443"
 
 #define IAM_USRPORTAL_APPNAME "IAM_USRPORTAL"
-#define IAM_USRPORTAL_DESCRIPTION "Identity Access Management User Portal And Login Page"
+#define IAM_USRPORTAL_DESCRIPTION "Identity Access Management User Portal"
 #define IAM_USRPORTAL_URL "https://iamuser.localhost:11443"
 
 #define IAM_LOGINPORTAL_DEF_WEBROOTDIR "/var/www/ufastauthd3/login"
